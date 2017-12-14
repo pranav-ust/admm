81c3635a49e55a604bb8f703fb7f494c6b7f92aa
Pranav A <worldly.music.cafe@gmail.com>
2017-12-14 20:14:02 +0800

    Conv

--- conv.py
+++ conv.py
@@ -6,11 +6,14 @@ k = np.array([[1,2],[3,4]])
 
 x = np.array([[a[0,0], a[0,1], a[1,0], a[1,1]], [a[0,1], a[0,2], a[1,1], a[1,2]], [a[1,0], a[1,1], a[2,0], a[2,1]], [a[1,1], a[1,2], a[2,1], a[2,2]]])
 x_dash = np.linalg.pinv(x)
-print(x_dash)
+# print(x_dash)
 
 y = signal.convolve2d(a, np.rot90(k, 2), 'valid')
 l = y.reshape((1,4))
-print(np.dot(l, x_dash))
+# print(np.dot(l, x_dash))
+#
+# print("Original kernel: ", k.reshape((1,4)))
+# print("Retrieved kernel: ", np.dot(l, x_dash))
 
-print("Original kernel: ", k.reshape((1,4)))
-print("Retrieved kernel: ", np.dot(l, x_dash))
+for i in range(3):
+	for j in range(2):
