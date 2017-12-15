553ad376dbc1c1154ff067746ced888724b34081
NeroJ <735422760@qq.com>
2017-12-15 23:15:49 +0800

    BaseLine

--- baseline.py
+++ baseline.py
@@ -1,129 +1,88 @@
-import matplotlib.pyplot as plt
-import numpy as np
-import sklearn
-import sklearn.datasets
-import sklearn.linear_model
-import matplotlib
 from sklearn.datasets import load_digits
-import matplotlib.pyplot as plt
 import numpy as np
-from skimage import transform,data
 
+class BaseLine:
+    def __init__(self):
+        self.digits = self.load_MINISTDATA_binary()
+        # input dataset & output dataset
+        self.X = self.digits['data']
+        self.y = self.digits['target']
+        # variables of input and output
+        self.num_examples = len(self.X)  # training set size
+        self.nn_input_dim = 64  # input layer dimensionality
+        self.nn_output_dim = 2  # output layer dimensionality
+        # Gradient descent parameters
+        self.epsilon = 0.01  # learning rate
+        self.reg_lambda = 0.01  # regularization strength
+        #----run the model------
+        run = self.build_model(3, print_loss=True)
 
-def load_MINISTDATA_binary():
-    digits = {}
-    sam = load_digits()
-    digits['data'] = sam.data
-    digits['target'] = np.array([1 if float(item) % float(2) == 0 else -1 for item in sam.target])
-    return digits
-
-digits = load_MINISTDATA_binary()
-# sigmoid function
-def nonlin(x,deriv=False):
-    if(deriv==True):
-        return x*(1-x)
-    return 1/(1+np.exp(-x))
-
-# input dataset
-X =  digits['data']
-# output dataset
-y = digits['target']
-# %% 4
-
-# %% 15
-num_examples = len(X) # training set size
-nn_input_dim = 64 # input layer dimensionality
-nn_output_dim = 2 # output layer dimensionality
-
-# Gradient descent parameters (I picked these by hand)
-epsilon = 0.01 # learning rate for gradient descent
-reg_lambda = 0.01 # regularization strength
-
-# %% 7
-# Helper function to evaluate the total loss on the dataset
-def calculate_loss(model):
-    W1, b1, W2, b2 = model['W1'], model['b1'], model['W2'], model['b2']
-    # Forward propagation to calculate our predictions
-    z1 = X.dot(W1) + b1
-    a1 = np.tanh(z1)
-    z2 = a1.dot(W2) + b2
-    exp_scores = np.exp(z2)
-    probs = exp_scores / np.sum(exp_scores, axis=1, keepdims=True)
-    # Calculating the loss
-    corect_logprobs = -np.log(probs[range(num_examples), y])
-    data_loss = np.sum(corect_logprobs)
-    # Add regulatization term to loss (optional)
-    data_loss += reg_lambda/2 * (np.sum(np.square(W1)) + np.sum(np.square(W2)))
-    return 1./num_examples * data_loss
-
-# %% 8
-# Helper function to predict an output (0 or 1)
-def predict(model, x):
-    W1, b1, W2, b2 = model['W1'], model['b1'], model['W2'], model['b2']
-    # Forward propagation
-    z1 = x.dot(W1) + b1
-    a1 = np.tanh(z1)
-    z2 = a1.dot(W2) + b2
-    exp_scores = np.exp(z2)
-    probs = exp_scores / np.sum(exp_scores, axis=1, keepdims=True)
-    return np.argmax(probs, axis=1)
-
-# %% 16
-# This function learns parameters for the neural network and returns the model.
-# - nn_hdim: Number of nodes in the hidden layer
-# - num_passes: Number of passes through the training data for gradient descent
-# - print_loss: If True, print the loss every 1000 iterations
-def build_model(nn_hdim, num_passes=20000, print_loss=False):
-
-    # Initialize the parameters to random values. We need to learn these.
-    np.random.seed(0)
-    W1 = np.random.randn(nn_input_dim, nn_hdim) / np.sqrt(nn_input_dim)
-    b1 = np.zeros((1, nn_hdim))
-    W2 = np.random.randn(nn_hdim, nn_output_dim) / np.sqrt(nn_hdim)
-    b2 = np.zeros((1, nn_output_dim))
-
-    # This is what we return at the end
-    model = {}
-
-    # Gradient descent. For each batch...
-    for i in range(0, num_passes):
-
-        # Forward propagation
-        z1 = X.dot(W1) + b1
+    #load data into binary classes
+    def load_MINISTDATA_binary(self):
+        digits = {}
+        sam = load_digits()
+        digits['data'] = sam.data
+        digits['target'] = np.array([1 if float(item) % float(2) == 0 else -1 for item in sam.target])
+        return digits
+    # calculate the loss
+    def calculate_loss(self, model):
+        W1, b1, W2, b2 = model['W1'], model['b1'], model['W2'], model['b2']
+        z1 = self.X.dot(W1) + b1
         a1 = np.tanh(z1)
         z2 = a1.dot(W2) + b2
         exp_scores = np.exp(z2)
         probs = exp_scores / np.sum(exp_scores, axis=1, keepdims=True)
+        # Calculating the loss
+        corect_logprobs = -np.log(probs[range(self.num_examples), self.y])
+        data_loss = np.sum(corect_logprobs)
+        # Add regulatization term to loss
+        data_loss += self.reg_lambda / 2 * (np.sum(np.square(W1)) + np.sum(np.square(W2)))
+        return 1. / self.num_examples * data_loss
 
-        # Backpropagation
-        delta3 = probs
-        delta3[range(num_examples), y] -= 1
-        dW2 = (a1.T).dot(delta3)
-        db2 = np.sum(delta3, axis=0, keepdims=True)
-        delta2 = delta3.dot(W2.T) * (1 - np.power(a1, 2))
-        dW1 = np.dot(X.T, delta2)
-        db1 = np.sum(delta2, axis=0)
+    def predict(self, model, x):
+        W1, b1, W2, b2 = model['W1'], model['b1'], model['W2'], model['b2']
+        z1 = x.dot(W1) + b1
+        a1 = np.tanh(z1)
+        z2 = a1.dot(W2) + b2
+        exp_scores = np.exp(z2)
+        probs = exp_scores / np.sum(exp_scores, axis=1, keepdims=True)
+        return np.argmax(probs, axis=1)
 
-        # Add regularization terms (b1 and b2 don't have regularization terms)
-        dW2 += reg_lambda * W2
-        dW1 += reg_lambda * W1
+    def build_model(self, nn_hdim, num_passes=20000, print_loss=False):
+        np.random.seed(0)
+        W1 = np.random.randn(self.nn_input_dim, nn_hdim) / np.sqrt(self.nn_input_dim)
+        b1 = np.zeros((1, nn_hdim))
+        W2 = np.random.randn(nn_hdim, self.nn_output_dim) / np.sqrt(nn_hdim)
+        b2 = np.zeros((1, self.nn_output_dim))
+        model = {}
+        for i in range(0, num_passes):
+            #-----Forward propagation-----------
+            z1 = self.X.dot(W1) + b1
+            a1 = np.tanh(z1)
+            z2 = a1.dot(W2) + b2
+            exp_scores = np.exp(z2)
+            probs = exp_scores / np.sum(exp_scores, axis=1, keepdims=True)
+            #------Backpropagation-------------
+            delta3 = probs
+            delta3[range(self.num_examples), self.y] -= 1
+            dW2 = (a1.T).dot(delta3)
+            db2 = np.sum(delta3, axis=0, keepdims=True)
+            delta2 = delta3.dot(W2.T) * (1 - np.power(a1, 2))
+            dW1 = np.dot(self.X.T, delta2)
+            db1 = np.sum(delta2, axis=0)
+            #----Add regularization terms--------
+            dW2 += self.reg_lambda * W2
+            dW1 += self.reg_lambda * W1
+            #----Gradient descent parameter update----
+            W1 += -self.epsilon * dW1
+            b1 += -self.epsilon * db1
+            W2 += -self.epsilon * dW2
+            b2 += -self.epsilon * db2
+            # Set new parameters
+            model = {'W1': W1, 'b1': b1, 'W2': W2, 'b2': b2}
+            if print_loss and i % 1000 == 0:
+                print("Loss after iteration %i: %f" % (i, self.calculate_loss(model)))
 
-        # Gradient descent parameter update
-        W1 += -epsilon * dW1
-        b1 += -epsilon * db1
-        W2 += -epsilon * dW2
-        b2 += -epsilon * db2
+        return model
 
-        # Assign new parameters to the model
-        model = { 'W1': W1, 'b1': b1, 'W2': W2, 'b2': b2}
-
-        # Optionally print the loss.
-        # This is expensive because it uses the whole dataset, so we don't want to do it too often.
-        if print_loss and i % 1000 == 0:
-          print("Loss after iteration %i: %f" %(i, calculate_loss(model)))
-
-    return model
-
-# %% 17
-# Build a model with a 3-dimensional hidden layer
-model = build_model(3, print_loss=True)
+ob = BaseLine()
